b9dc2eadb424d093b5d8151ef613075c7e79f409
Mujahid Siyam <mujaxso@proton.me>
2025-10-18 20:29:34 +0200

    fix: ligature not showing

--- modules/ui/funmacs-ligature.el
+++ modules/ui/funmacs-ligature.el
@@ -8,6 +8,12 @@
 (use-package ligature
   :ensure t
   :config
+  ;; Enable the "www" ligature in every possible major mode
+  (ligature-set-ligatures 't '("www"))
+  ;; Enable traditional ligature support in eww-mode, if the
+  ;; `variable-pitch' face supports it
+  (ligature-set-ligatures 'eww-mode '("ff" "fi" "ffi"))
+  ;; Enable ligature in programming mode
   (ligature-set-ligatures 'prog-mode '("--" "---" "==" "===" "!=" "!==" "=!="
 				       "=:=" "=/=" "<=" ">=" "&&" "&&&" "&=" "++" "+++" "***" ";;" "!!"
 				       "??" "???" "?:" "?." "?=" "<:" ":<" ":>" ">:" "<:<" "<>" "<<<" ">>>"
