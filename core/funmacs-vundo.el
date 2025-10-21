de45a9755c547b3deabc8990454ed1859bab0f26
Mujahid Siyam <mujaxso@proton.me>
2025-10-21 19:58:30 +0200

    fix: vundo undo limits

--- core/funmacs-vundo.el
+++ core/funmacs-vundo.el
@@ -7,7 +7,13 @@
 
 (use-package vundo
   :bind (("C-x u" . vundo))
-  :config (setq vundo-compact-display t))
+  :config
+  (setq vundo-compact-display t)
+  ;; Increase undo limits to prevent truncation
+  (setq undo-limit 800000)           ;; default: ~160k
+  (setq undo-strong-limit 12000000)  ;; default: ~240k
+  (setq undo-outer-limit 120000000)  ;; default: ~24MB
+  )
 
 (provide 'funmacs-vundo)
 
