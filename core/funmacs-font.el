d6ddf943eb72a32eec56c20bea2993c6a27644f7
Mujahid Siyam <mujaxso@proton.me>
2025-10-24 17:37:57 +0200

    add: default font jetbrains nerd font

--- core/funmacs-font.el
+++ core/funmacs-font.el
@@ -6,8 +6,12 @@
 
 ;;; code
 
-(when (member "FiraCode Nerd Font" (font-family-list))
-  (set-face-attribute 'default nil :font "FiraCode Nerd Font" :height 120))
+(when (member "JetBrainsMono Nerd Font" (font-family-list))
+  (set-face-attribute 'default nil :family "JetBrainsMono Nerd Font" :height 120)
+  (set-face-attribute 'fixed-pitch nil :family "JetBrainsMono Nerd Font" :height 1.0)
+  (set-face-attribute 'variable-pitch nil :family "Inter" :height 1.0))
+
+
 
 (provide 'funmacs-font)
 
