9b0dcdce9ed83536f050c9fd1d37117ae8b5511c
Mujahid Siyam <mujaxso@proton.me>
2025-10-25 15:41:19 +0200

    fix: vundo window with meow

--- core/funmacs-vundo.el
+++ core/funmacs-vundo.el
@@ -6,15 +6,32 @@
 ;;; code
 
 (use-package vundo
-  :bind (("C-x u" . vundo))
+  :bind
+  (("C-x u" . funmacs/vundo-open)
+   (:map meow-normal-state-keymap
+         ("u" . funmacs/vundo-open)))
   :config
-  (setq vundo-compact-display t)
-  ;; Increase undo limits to prevent truncation
+  ;; Appearance
+  (setq vundo-glyph-alist vundo-unicode-symbols
+        vundo-compact-display t)
+
+  ;; Undo limits
   (setq undo-limit 800000)           ;; default: ~160k
-  (setq undo-strong-limit 12000000)  ;; default: ~240k
-  (setq undo-outer-limit 120000000)  ;; default: ~24MB
-  )
+  (setq undo-strong-limit 12000000)  ;; default: ~12MB
+  (setq undo-outer-limit 120000000)  ;; default: ~120MB
+
+  ;; --- Fixed: prevent dedicated window error ---
+  (defun funmacs/vundo-open ()
+    "Open vundo in a non-dedicated window."
+    (interactive)
+    (let ((display-buffer-alist
+           '(("\\*vundo-tree\\*"
+              ;; Open in a normal, non-dedicated side window
+              (display-buffer-in-side-window)
+              (side . right)
+              (window-width . 0.3)
+              (dedicated . nil)))))
+      (vundo))))
 
 (provide 'funmacs-vundo)
-
 ;;; funmacs-vundo.el ends here
